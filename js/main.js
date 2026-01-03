d6d391e614d86cc33d2516dafd1ecd130f34afd4
Andrew Smirnov <t.rainbow.ray@gmail.com>
2026-01-03 21:27:04 +0300

    add burger

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
-(function() {
-	// Your code here
-	const header = document.querySelector('.header');
+(function () {
+  const header = document.querySelector('.header');
 
-	window.onscroll = () => {
-		if (window.pageYOffset > 50) {
-			header.classList.add('header_active');
-		} else {
-			header.classList.remove('header_active');
-		}
-	};
+  window.onscroll = () => {
+    if (window.pageYOffset > 50) {
+      header.classList.add('header--active');
+    } else {
+      header.classList.remove('header--active');
+    }
+  };
+})();
+
+// Burger handler
+(function () {
+  const burgerItem = document.querySelector('.burger');
+  const menu = document.querySelector('.header__nav');
+  const menuCloseItem = document.querySelector('.header__nav-close');
+
+  burgerItem.addEventListener('click', () => {
+    menu.classList.add('header__nav--active');
+  });
+  menuCloseItem.addEventListener('click', () => {
+    menu.classList.remove('header__nav--active');
+  });
 })();
